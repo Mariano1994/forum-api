2c5ff0b44ff9b29f4513bb5931486a932b1bb68a
Mariano Capiliku <marianocapiliku@gmail.com>
2025-11-06 10:29:59 +0100

    feat: Created aplications use cases, entities and the answer question use case test

---
+++ src/domain/use-cases/answer-question.test.ts
@@ -0,0 +1,18 @@
+import {describe, expect, it} from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+
+
+describe('Answer question use case', ()=> {
+
+  it('should create an Answer', async ()=> {
+    const answerQuestion = new AnswerQuestionUseCase()
+
+    const answer =answerQuestion.handler({
+      instructorId: '1',
+      questionId: '1',
+      content: 'New Answer'
+    })
+
+    expect(answer.content).toEqual('New Answer')
+  })
+})
